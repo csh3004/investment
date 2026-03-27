ec7220585b60ea4f3a9ef538187987482113527c
hoon <csh3004@naver.com>
2026-03-27 21:44:54 +0900

    26.03.27 프론트 디렉토리 지정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './features/auth/Login'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login/>
   </React.StrictMode>
 );
 
