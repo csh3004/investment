63b1134c94612caadbcb999d2a424ae9ddff1311
tunachoi-dev <choidongwon9929@gmail.com>
2026-04-01 22:24:50 +0900

    feat: 로그인 UI 개편

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import LoginForm from '../../features/auth/Login';
+import { Login } from '../../features/auth/Login';
 
 const LoginPage: React.FC = () => {
   return (
-    // 페이지 수준의 레이아웃 설정 (배경색, 중앙 정렬 등)
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <LoginForm />
+    <div className="w-full h-full">
+      <Login />
     </div>
   );
 };
