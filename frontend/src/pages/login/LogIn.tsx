0731c4e1fb29dee53e0d51d066d8b9504e523bf5
hoon <csh3004@naver.com>
2026-04-04 04:42:49 +0900

    fix: 충돌 해결

---
+++ frontend/src/pages/login/LogIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LogIn } from '../../features/auth/LogIn';
+
+
+const LogInPage: React.FC = () => {
+  return (
+    <div className="w-full h-full">
+      <LogIn />
+    </div>
+  );
+};
+
+export default LogInPage;
