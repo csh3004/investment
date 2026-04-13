e7f50e9e4b1ba3430bd17afd44009544620e57e8
shchoi <shchoi@smartmaker.com>
2026-04-13 10:18:35 +0900

    feat/transaction 추가
    - 기존 메인페이지 가져와서 가공 예정

---
+++ frontend/src/pages/transaction/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TransationForm from '../../features/transaction/Transaction';
+
+const Transaction: React.FC = () => {
+  return (
+    <div className="w-full h-full">
+      <TransationForm />
+    </div>
+  );
+};
+
+export default Transaction;
