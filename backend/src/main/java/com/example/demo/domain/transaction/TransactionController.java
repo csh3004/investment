62c0de19972abfa81368c5ddbdc1a28b090fb78d
hoon <csh3004@naver.com>
2026-04-14 21:01:34 +0900

    feat/back/transaction init
    - 컨트롤러 생성
    - 프론트 전달 파라미터 결정 후 구현 예정

---
+++ backend/src/main/java/com/example/demo/domain/transaction/TransactionController.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.transaction;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+
+}
